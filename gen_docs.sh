2b3917adc8e35a7bea8888595b1015ec1c48b7fb
Aryan Sharma <sharmaisgreat46@gmail.com>
2022-11-27 03:59:48 +0530

    Added executors to execute some og aegons features

---
+++ gen_docs.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# 'set -e' stops the execution of a script if a command or pipeline has an error.
+# This is the opposite of the default shell behaviour, which is to ignore errors in scripts.
+set -e
+
+branch="$(git rev-parse --abbrev-ref HEAD)"
+checker=$(git diff --name-only "$(git merge-base $branch HEAD)")
+if [[ ! $checker =~ "version.py" ]]; then
+  echo -e "\n********************************************************************ERROR********************************************************************"
+  echo "Docs generation was ABORTED since module version was not updated!! Changelog generator requires the commit number and package version in sync."
+  echo -e "*********************************************************************************************************************************************\n"
+  exit 255
+fi
+
+if [[ ! $checker =~ "CHANGELOG" ]]; then
+  echo -e "\n********************************************************************ERROR********************************************************************"
+  echo "Docs generation was ABORTED since CHANGELOG was not updated!! Changelog generator requires the commit number and package version in sync."
+  echo -e "*********************************************************************************************************************************************\n"
+  exit 255
+fi
+
+rm -rf docs
+mkdir docs
+mkdir -p doc_generator/_static  # creates a _static folder if unavailable
+cp README.md doc_generator && cd doc_generator && make clean html && mv _build/html/* ../docs && rm -rf README.md fileio logs
+touch ../docs/.nojekyll
